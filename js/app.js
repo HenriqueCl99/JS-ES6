ee6712e023ffa82d665dae6e215a1f246eaa7525
Antonio Henrique <ahenriquel1999@gmail.com>
2023-04-13 20:22:37 -0300

    Adicionando arquivo js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let variavel = 1;
+
+let nome = 'Antonio';
+
+switch(nome) {
+    case 'Henrique':
+        console.log('Esse é o sobrenome');
+    break;
+
+    case 'Antonio':
+        console.log('Esse é o nome');
+    break;
+
+    case 'Calixto':
+        console.log('Esse é o terceiro nome');
+    break;
+
+    default:
+        console.log('Essa é a exceção');
+}
